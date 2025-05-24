bd29e5b5614d52cf4b8baf9217cf217087a192db
TheLonlyKnight <mhd.wanli@elev.ga.ntig.se>
2025-05-24 19:50:05 +0200

    idk if work

--- sistaUppgift/jsscript/product.js
+++ sistaUppgift/jsscript/product.js
@@ -2,19 +2,19 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get('id');const products = {
     "1": {
-        image: "/bilder/smallareBilder/Skärmbild 2025-05-06 153101.webp",
+        image: "../bilder/smallareBilder/thirdproduct.webp",
         title: "Fancy Chess Board",
         description: "Is there an important date coming soon and you don't know what gift to give? No worries! For only $499, you can gift a full chess board.",
         price: "Price: $499.99"
     },
     "2": {
-        image: "/bilder/smallareBilder/secondproduct.webp",
+        image: "../bilder/smallareBilder/secondproduct.webp",
         title: "Arabian Chess Board",
         description: "For only $599, you can get a full chess set Arabian style with gold paint.",
         price: "Price: $599.99"
     },
     "3": {
-        image: "/bilder/smallareBilder/firstproduct (1).webp",
+        image: "../bilder/smallareBilder/firstproduct.webp",
         title: "Certified Chess Board",
         description: "For only $399, you can get your hands on a certified chess board from ABI Not chess.com.",
         price: "Price: $399.99"
